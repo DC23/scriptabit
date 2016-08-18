b7b954e772b86d450c01f5392fe644e40830c807
DC23 <jugglindan@gmail.com>
2016-08-18 15:49:34 +0800

    Bump version: 1.1.0 → 1.2.0

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ class PyTest(TestCommand):
 
 setup(
     name='scriptabit',
-    version='1.1.0',
+    version='1.2.0',
     description='Python scripting for Habitica via the API',
     long_description=long_description,
     url='https://github.com/DC23/scriptabit',
