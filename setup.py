164862150e1bf4c9114a65b6af27d86e171e4c9a
Daniel Collins <jugglindan@gmail.com>
2017-01-17 11:42:18 +0800

    Bump version: 1.17.0 → 1.17.1

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ class PyTest(TestCommand):
 
 setup(
     name='scriptabit',
-    version='1.17.0',
+    version='1.17.1',
     description='Python scripting for Habitica via the API',
     long_description=long_description,
     url='https://github.com/DC23/scriptabit',
