eddb571c4e19360c1f1763bda80557bd9adcb8b7
DC23 <jugglindan@gmail.com>
2018-04-17 12:07:53 +0800

    Bump version: 2.0.2 → 2.1.0

--- docs/index.rst
+++ docs/index.rst
@@ -13,7 +13,7 @@ Python scripting and scenarios for Habitica.
 * Free software: Apache 2.0
 * Homepage: https://github.com/DC23/scriptabit
 * Documentation: https://scriptabit.readthedocs.org
-* Version: 2.0.2
+* Version: 2.1.0
 
 Contents
 --------
--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ class PyTest(TestCommand):
 
 setup(
     name='scriptabit',
-    version='2.0.2',
+    version='2.1.0',
     description='Python scripting for Habitica via the API',
     long_description=long_description,
     url='https://github.com/DC23/scriptabit',
